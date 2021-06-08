0905a61448045fad0831dfef9d46f3ffd22af39d
Chris Ruppel <ruppel@reliefweb.int>
2021-06-08 15:40:19 +0200

    [SNAP-57] install ESLint and adopt HID configuration

---
+++ app/.eslintrc.js
@@ -0,0 +1,23 @@
+module.exports = {
+  env: {
+    browser: false,
+    es6: true,
+  },
+  extends: 'airbnb-base',
+  globals: {
+    Atomics: 'readonly',
+    SharedArrayBuffer: 'readonly',
+  },
+  parserOptions: {
+    ecmaVersion: 2018,
+    sourceType: 'module',
+  },
+  rules: {
+    "no-plusplus": "off",
+    "no-underscore-dangle": "off",
+    "prefer-destructuring": "off",
+    "import/no-unresolved": [2, {
+      ignore: ['newrelic'],
+    }],
+  },
+};
