65e69916c8445f20f100e32721f44fea7eff7eda
Peter Lieverdink <lieverdink@reliefweb.int>
2022-11-10 15:30:00 +1100

    fix: Correct ones syntax. I donno about the "assigned value and never used". Maybe make this not a module, then?

--- app/apm.js
+++ app/apm.js
@@ -4,8 +4,8 @@
 const apm = require('elastic-apm-node').start({
   serviceName: process.env.ELASTIC_APM_SERVICE,
   secretToken: process.env.ELASTIC_APM_TOKEN,
-  serverUrl:   process.env.ELASTIC_APM_SERVER_URL,
-  environment: process.env.STAGE
-})
+  serverUrl: process.env.ELASTIC_APM_SERVER_URL,
+  environment: process.env.STAGE,
+});
 
 module.exports = apm;
